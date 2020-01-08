b09f76d2d24ee1f0bf481b15bfd38e0f353e27e8
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-01-09 01:11:26 +0900

    카드 덱을 의미하는 Deck 클래스를 생성 1. Deck class는 카드 리스트를 갖고 있으며, 생성 시점에 자동으로 52장의 서로 다른 트럼프 카드를 만든다.

---
+++ src/GhostFriend/Base/Deck/Deck.java
@@ -0,0 +1,30 @@
+package GhostFriend.Base.Deck;
+
+import GhostFriend.Base.Card.Card;
+import GhostFriend.Base.Card.CardSuit;
+import GhostFriend.Base.Card.CardValue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+    private List<Card> cardList;
+
+    private List<Card> CreateDeck() {
+        List<Card> cardList = new ArrayList<>();
+
+        for (CardSuit suit: CardSuit.values()) {
+            for (CardValue value: CardValue.values()) {
+                Card card = new Card(suit, value);
+
+                cardList.add(card);
+            }
+        }
+
+        return cardList;
+    }
+
+    public Deck() {
+        this.cardList = CreateDeck();
+    }
+}
--- src/GhostFriend/Base/Main.java
+++ src/GhostFriend/Base/Main.java
@@ -1,8 +1,12 @@
 package GhostFriend.Base;
 
+import GhostFriend.Base.Deck.Deck;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Deck deck = new Deck();
     }
 }
