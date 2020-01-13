1d48a018d544d38b910f79bc4d8d31ca77f97016
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-01-13 21:02:42 +0900

    RuleTest 생성 - Rule 클래스를 테스트하기 위한 테스트 클래스 생성

---
+++ UnitTest/GhostFriend/Base/Rule/RuleTest.java
@@ -0,0 +1,13 @@
+package GhostFriend.Base.Rule;
+
+import org.junit.BeforeClass;
+
+class RuleTest {
+
+    private static Rule rule;
+
+    @BeforeClass
+    public static void CreateRule() {
+        rule = new Rule();
+    }
+}
