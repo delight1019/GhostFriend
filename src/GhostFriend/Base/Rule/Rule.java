b5f4a25636b38843629908bbec9d8d6d2fe335c1
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-01-18 13:22:27 +0900

    Rule 클래스에 Mighty, JokerCall, Giru property 추가

--- src/GhostFriend/Base/Rule/Rule.java
+++ src/GhostFriend/Base/Rule/Rule.java
@@ -1,14 +1,50 @@
 package GhostFriend.Base.Rule;
 
 import GhostFriend.Base.Card.Card;
+import GhostFriend.Base.Card.CardSuit;
+import GhostFriend.Base.Card.CardValue;
 
 public class Rule {
+    private Card Mighty;
+    private Card JokerCall;
+    private CardSuit Giru;
+
+    public Card getMighty() {
+        return Mighty;
+    }
+
+    public void setMighty(Card mighty) {
+        Mighty = mighty;
+    }
+
+    public Card getJokerCall() {
+        return JokerCall;
+    }
+
+    public void setJokerCall(Card jokerCall) {
+        JokerCall = jokerCall;
+    }
+
+    public CardSuit getGiru() {
+        return Giru;
+    }
+
+    public void setGiru(CardSuit giru) {
+        Giru = giru;
+    }
+
     private Integer getScore(Card card) {
         if (card.IsScoreCard()) {
             return 1;
         } else {
             return 0;
         }
+    }
+
+    public Rule() {
+        this.Mighty = new Card(CardSuit.SPADE, CardValue.ACE);
+        this.JokerCall = new Card(CardSuit.CLUB, CardValue.THREE);
+        this.Giru = CardSuit.SPADE;
     }
 
 }
