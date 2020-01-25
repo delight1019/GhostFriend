4d89040448a0618c44ace174573173df85c96fac
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-01-25 23:00:43 +0900

    Rule 클래스의 Test Case 추가

--- UnitTest/GhostFriend/Base/Rule/RuleTest.java
+++ UnitTest/GhostFriend/Base/Rule/RuleTest.java
@@ -1,9 +1,15 @@
 package GhostFriend.Base.Rule;
 
+import GhostFriend.Base.Card.Card;
 import GhostFriend.Base.Card.CardSuit;
+import GhostFriend.Base.Card.CardValue;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RuleTest {
     private Rule rule = new Rule();
@@ -31,5 +37,27 @@ class RuleTest {
 
         newContract.declare(CardSuit.SPADE, 17);
         assertEquals(ContractValidation.VALID, rule.isValidContract(currentContract, newContract));
+    }
+
+    @Test
+    void decreaseMinContractScore() {
+        rule.decreaseMinContractScore();
+        assertEquals(12, rule.getMinContractScore());
+    }
+
+    @Test
+    void isDealMiss() {
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card(CardSuit.SPADE, CardValue.ACE));
+        cardList.add(new Card(CardSuit.SPADE, CardValue.TEN));
+        cardList.add(new Card(CardSuit.SPADE, CardValue.SEVEN));
+        cardList.add(new Card(CardSuit.SPADE, CardValue.EIGHT));
+        cardList.add(new Card(CardSuit.SPADE, CardValue.TWO));
+        cardList.add(new Card(CardSuit.SPADE, CardValue.THREE));
+        cardList.add(new Card(CardSuit.SPADE, CardValue.JACK));
+        cardList.add(new Card(CardSuit.JOKER, CardValue.JOKER));
+        cardList.add(new Card(CardSuit.SPADE, CardValue.FIVE));
+        cardList.add(new Card(CardSuit.SPADE, CardValue.SIX));
+        assertTrue(rule.isDealMiss(cardList));
     }
 }
