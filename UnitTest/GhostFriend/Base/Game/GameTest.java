7812605ec643f27048556415912a044c85f26e75
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-03-20 23:41:52 +0900

    Create Unit Test file for Game class

---
+++ UnitTest/GhostFriend/Base/Game/GameTest.java
@@ -0,0 +1,22 @@
+package GhostFriend.Base.Game;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    @Test
+    void isAllPlayersEntered() {
+    }
+
+    @Test
+    void getPlayersInfo() {
+    }
+
+    @Test
+    void addPlayer() {
+    }
+
+    @Test
+    void removePlayer() {
+    }
+}
