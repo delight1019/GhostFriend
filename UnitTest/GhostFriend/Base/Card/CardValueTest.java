ce2fde16b080565374d2c3176c7d5213f3ea48eb
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-04-25 12:17:14 +0900

    [Test] CardValue TestCase 추가
    Signed-off-by: Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>

--- UnitTest/GhostFriend/Base/Card/CardValueTest.java
+++ UnitTest/GhostFriend/Base/Card/CardValueTest.java
@@ -3,6 +3,7 @@ package GhostFriend.Base.Card;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class CardValueTest {
 
@@ -11,5 +12,18 @@ class CardValueTest {
         assertEquals(CardValue.ACE, CardValue.convertString("ace"));
         assertEquals(CardValue.ACE, CardValue.convertString("Ace"));
         assertEquals(CardValue.ACE, CardValue.convertString("ACE"));
+        assertEquals(CardValue.TWO, CardValue.convertString("two"));
+        assertEquals(CardValue.THREE, CardValue.convertString("three"));
+        assertEquals(CardValue.FOUR, CardValue.convertString("four"));
+        assertEquals(CardValue.FIVE, CardValue.convertString("five"));
+        assertEquals(CardValue.SIX, CardValue.convertString("six"));
+        assertEquals(CardValue.SEVEN, CardValue.convertString("seven"));
+        assertEquals(CardValue.EIGHT, CardValue.convertString("eight"));
+        assertEquals(CardValue.NINE, CardValue.convertString("nine"));
+        assertEquals(CardValue.TEN, CardValue.convertString("ten"));
+        assertEquals(CardValue.JACK, CardValue.convertString("jack"));
+        assertEquals(CardValue.QUEEN, CardValue.convertString("queen"));
+        assertEquals(CardValue.KING, CardValue.convertString("king"));
+        assertNull(CardValue.convertString("joker"));
     }
 }
