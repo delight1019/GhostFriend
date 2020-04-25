1742b202e04acf4e6b53d34940eb7a8e45404923
Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>
2020-04-25 12:20:14 +0900

    [Test] CardSuit TestCase 추가
    Signed-off-by: Hyehyeon-Kim <dev.hyehyeon.kim@gmail.com>

--- UnitTest/GhostFriend/Base/Card/CardSuitTest.java
+++ UnitTest/GhostFriend/Base/Card/CardSuitTest.java
@@ -3,6 +3,7 @@ package GhostFriend.Base.Card;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class CardSuitTest {
 
@@ -12,5 +13,9 @@ class CardSuitTest {
         assertEquals(CardSuit.SPADE, CardSuit.convertString("spade"));
         assertEquals(CardSuit.JOKER, CardSuit.convertString("JOKER"));
         assertEquals(CardSuit.JOKER, CardSuit.convertString("joker"));
+        assertEquals(CardSuit.DIAMOND, CardSuit.convertString("diamond"));
+        assertEquals(CardSuit.CLUB, CardSuit.convertString("club"));
+        assertEquals(CardSuit.HEART, CardSuit.convertString("heart"));
+        assertNull(CardSuit.convertString("Invalid"));
     }
 }
