90494b3c5328dbd62c5f9a0d4ef7871909a54912
hyehyeon.kim <kippem9088@gmail.com>
2022-06-08 22:56:41 +0900

    [Fix] 시작할 때 Log 폴더가 없을 경우 생성하도록 수정

--- src/GhostFriend/Utils/Log.java
+++ src/GhostFriend/Utils/Log.java
@@ -3,22 +3,52 @@ package GhostFriend.Utils;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 
 public class Log {
+    private static final String dir = "Log";
+    private static final String fileName = "Server.log";
     private static File file;
-    private static FileWriter fileWriter;
 
-    public static synchronized void printText(String text) throws IOException {
+    private static synchronized void createDir() {
+        try {
+            Path dirPath = Paths.get(dir);
+            Files.createDirectories(dirPath);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static synchronized void createFile() {
         if (file == null) {
-            file = new File("Log/ServerLog.txt");
+            file = new File(dir + "/" + fileName);
         }
 
-        fileWriter = new FileWriter(file, true);
+        if (file.exists()) {
+            return;
+        }
+
+        try {
+            if (!file.createNewFile()) {
+                throw new IOException("File not created");
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized void printText(String text) throws IOException {
+        createDir();
+        createFile();
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd HH:mm:ss");
 
-        try {
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
             fileWriter.write(simpleDateFormat.format(System.currentTimeMillis()));
             fileWriter.write(" (" + Thread.currentThread().getId() + ")");
             fileWriter.write(" ---- ");
@@ -30,11 +60,6 @@ public class Log {
         catch (IOException e) {
             e.printStackTrace();
         }
-
-        finally {
-            fileWriter.close();
-        }
-
 
         System.out.println(text);
     }
